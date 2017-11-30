ccfd6aaf74604048a68b04c4d11682a0d1b198ad
Marcus Hansson <machalvan@hotmail.com>
2017-11-30 15:21:09 +0100

    Added window with button

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("test")
+from tkinter import *
+
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("IOT")
+        self.pack(fill=BOTH, expand=1)
+
+        import_button = Button(self, text="Import", command=self.import_data)
+        import_button.place(x=0, y=0)
+
+    def import_data(self):
+        print("Import button was pressed!")
+
+root = Tk()
+root.geometry("400x300")
+
+app = Window(root)
+
+root.mainloop()
