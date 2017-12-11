c7ccf132b765e252fef6a5aeecb2f2c45894625d
Leatificat <sebbgar@gmail.com>
2017-12-11 18:09:37 +0100

    Skapat ett frame network som kan arbeta med varandra någorlunda bra iaf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import filedialog
 import matplotlib
 matplotlib.use("TkAgg")
@@ -8,26 +9,71 @@ from matplotlib.figure import Figure
 import csv
 
 
+class Window(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        #self.master = master
+        #self.init_window()
+
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.actT = []
+        self.actH = []
+        self.actB = []
+
+        self.frames = {}
+
+        for F in (StartPage, LinePage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def upLists(self, tem, hum, bri):
+        print("upplist")
+        self.actT = tem
+        self.actH = hum
+        self.actB = bri
+
+    def getT(self):
+        print("getT")
+        return self.actT
+
+    def getH(self):
+        return self.actH
+
+    def getB(self):
+        return self.actB
 
 
-class Window(Frame):
-
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-
-
-        self.master = master
-        self.init_window()
-
-    def init_window(self):
-        self.master.title("IOT")
-        self.pack(fill=BOTH, expand=1)
+    #def init_window(self):
+        #self.master.title("IOT")
+        #self.pack(fill=BOTH, expand=1)
 
         #self.canvas = Canvas(self, width=800, height=600)
         #self.canvas.place(x=100, y=0)
 
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = Label(self, text="Start Page")
+        label.pack(pady=10, padx=10)
+
         import_button = Button(self, text="Import", command=self.import_data, cursor="hand2")
-        import_button.place(x=0, y=0)
+        import_button.pack()
 
     def import_data(self):
         print("Import button was pressed!")
@@ -51,28 +97,45 @@ class Window(Frame):
             print("fyllt lista")
             print(temperature)
 
-            self.actTemp = temperature
-            self.actHumi = humidity
-            self.actBrig = brightness
+            self.controller.upLists(temperature, humidity, brightness)
 
-            self.linear_button = Button(self, text="Linear", command=self.linear())
-            self.linear_button.place(x=40, y=40)
+        self.controller.show_frame(LinePage)
+
+
+
+
+class LinePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = Label(self, text="Linear graph")
+        label.pack()
+
+        self.actT = []
+        self.actH = []
+        self.actB = []
+
+        buttonTest = tk.Button(self, text="test", command=lambda: self.linear())
+        buttonTest.pack()
 
     def linear(self):
-        print("test")
-        fig = Figure(figsize=(5,5), dpi=100)
+        self.getLists()
+        fig = Figure(figsize=(5, 5), dpi=100)
         plot = fig.add_subplot(111)
-        plot.plot([self.actTemp], [self.actBrig], 'ro')
+        plot.plot([self.actT], [self.actB], 'ro')
 
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.show()
         canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
 
+    def getLists(self):
+        self.actT = self.controller.getT()
+        self.actH = self.controller.getH()
+        self.actB = self.controller.getB()
+
+#root = Tk()
 
 
-root = Tk()
-root.geometry("600x400")
-
-app = Window(root)
-
-root.mainloop()
+app = Window()
+app.geometry("600x400")
+app.mainloop()
