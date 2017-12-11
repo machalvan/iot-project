78d7e57423aeda7009a77c9e0016ac08c4b69968
Leatificat <sebbgar@gmail.com>
2017-12-11 01:40:10 +0100

    Har lagt till möjlighet att plotta upp en linear graph, dock plottar den utan att man trycker på plot knappen så måste lösa det, sen måste även delar bli fixade så det ser snyggare ut

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from tkinter import *
 from tkinter import filedialog
+import matplotlib
+matplotlib.use("TkAgg")
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 import csv
+
+
 
 
 class Window(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
+
 
         self.master = master
         self.init_window()
@@ -15,7 +23,10 @@ class Window(Frame):
         self.master.title("IOT")
         self.pack(fill=BOTH, expand=1)
 
-        import_button = Button(self, text="Import", command=self.import_data)
+        #self.canvas = Canvas(self, width=800, height=600)
+        #self.canvas.place(x=100, y=0)
+
+        import_button = Button(self, text="Import", command=self.import_data, cursor="hand2")
         import_button.place(x=0, y=0)
 
     def import_data(self):
@@ -39,11 +50,28 @@ class Window(Frame):
                 brightness.append(brigV)
             print("fyllt lista")
             print(temperature)
-            print("test")
+
+            self.actTemp = temperature
+            self.actHumi = humidity
+            self.actBrig = brightness
+
+            self.linear_button = Button(self, text="Linear", command=self.linear())
+            self.linear_button.place(x=40, y=40)
+
+    def linear(self):
+        print("test")
+        fig = Figure(figsize=(5,5), dpi=100)
+        plot = fig.add_subplot(111)
+        plot.plot([self.actTemp], [self.actBrig], 'ro')
+
+        canvas = FigureCanvasTkAgg(fig, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+
 
 
 root = Tk()
-root.geometry("400x300")
+root.geometry("600x400")
 
 app = Window(root)
 
